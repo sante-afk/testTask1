d88ea1077f10cae27c7d4d25849d6b0da7c47c1a
TkHex <hcelovek2842@gmail.com>
2026-04-27 15:02:15 +0300

    Add bind action, enable action, disable action

--- input-controller.js
+++ input-controller.js
@@ -4,18 +4,45 @@ export class inputController {
         this.ACTION_RIGHT = ACTION_RIGHT;
         this.ACTION_UP = ACTION_UP;
         this.ACTION_DOWN = ACTION_DOWN;
-        
+
+        this.ACTION_ACTIVATED = false;
     }
 
     bindAction(actionsToBind) {
+        if (actionsToBind.keyCode === 37 || actionsToBind.keyCode === 65) {
+            this.ACTION_LEFT = actionsToBind.keyCode;
+            this.enableAction(actionsToBind);
+        } else if (actionsToBind.keyCode === 39 || actionsToBind.keyCode === 68) {
+            this.ACTION_RIGHT = actionsToBind.keyCode;
+            this.enableAction(actionsToBind);
+        } else if (actionsToBind.keyCode === 38 || actionsToBind.keyCode === 87) {
+            this.ACTION_UP = actionsToBind.keyCode;
+            this.enableAction(actionsToBind);
+        } else if (actionsToBind.keyCode === 40 || actionsToBind.keyCode === 83) {
+            this.ACTION_DOWN = actionsToBind.keyCode;
+            this.enableAction(actionsToBind);
+        }
+       
     }
 
     enableAction(actionName) {
-        
+        const input = document.getElementById("input1");
+
+        input.addEventListener("focusin", (event) => {
+            event.target.style.background = "black"
+        });
+
+        console.log(actionName.code);
     }
 
     disableAction(actionName) {
+        const input = document.getElementById("input1");
 
+        input.addEventListener("focusout", (event) => {
+            event.target.style.background = "white"
+        });
+
+        console.log(actionName.code);
     }
 
     attach(target, dontEnable) {
@@ -32,19 +59,15 @@ export class inputController {
 
     isKeyPressed(keyCode) {
         document.addEventListener("keydown", (keyCode) => {
-            if (keyCode.code === "ArrowLeft") {
-                console.log("Влево");
-            }
-            if (keyCode.key === "ArrowRight") {
-                console.log("Вправо");
-            }
-            if (keyCode.key === "ArrowUp") {
-                console.log("Вверх");
-            }
-            if (keyCode.key === "ArrowDown") {
-                console.log("Вниз");
-            }
+            this.ACTION_ACTIVATED = true;
+            console.log("Button activated: " + this.ACTION_ACTIVATED);
+            this.enableAction(keyCode);
         });
+        document.addEventListener("keyup", (keyCode) => {
+            this.ACTION_ACTIVATED = false;
+            console.log("Button diactivated: " + this.ACTION_ACTIVATED);
+            this.disableAction(keyCode);
+        })
         return Boolean;
     }
 
