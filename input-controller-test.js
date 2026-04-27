cd900fca4eb6c39657b3d1933d70cf6c8ace49d1
TkHex <hcelovek2842@gmail.com>
2026-04-27 11:39:40 +0300

    first commit

---
+++ input-controller-test.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    
+}
---
+++ input-controller.js
@@ -0,0 +1,36 @@
+export class inputController {
+    constructor() {
+
+    }
+
+    bindAction(actionsToBind) {
+
+    }
+
+    enableAction(actionName) {
+
+    }
+
+    disableAction(actionName) {
+
+    }
+
+    attach(target, dontEnable) {
+
+    }
+
+    detach() {
+
+    }
+
+    isActionActive(action) {
+        return Boolean;
+    }
+
+    isKeyPressed(keyCode) {
+
+        return Boolean;
+    }
+
+
+}
