a5b0cf2aa9839e2159ae61e9217091fef7a440cf
TkHex <hcelovek2842@gmail.com>
2026-04-29 16:14:22 +0300

    Fix issues

--- input-controller.js
+++ input-controller.js
@@ -7,11 +7,12 @@ static ACTION_DEACTIVATED = "input-controller:action-deactivated";
         this.ACTION;
         this.ACTION_LISTENER = false;
         this.ACTION_ACTIVE = false;
+        this.ACTION_PRESSED = false;
     }
 
     bindAction(actionsToBind) {
         this.ACTION = actionsToBind;
-        this.enableAction(actionsToBind.type);
+        const isActiv = this.isActionActive(actionsToBind.type) ? this.enableAction(actionsToBind.type) : this.disableAction(actionsToBind.type);
     }
 
     enableAction(actionName) {
@@ -19,14 +20,18 @@ static ACTION_DEACTIVATED = "input-controller:action-deactivated";
         const activate = document.getElementById("activate_place");
         const actionActive = this.isActionActive(actionName);
 
-        if (actionActive) {
+        if (actionActive === "keydown") {
             input.placeholder = actionName;
             const eventActivate = new Event(inputController.ACTION_ACTIVATED);
             this.ACTION.target.dispatchEvent(eventActivate, {bubbles: true});
             this.attach(this.ACTION);
-        } else {
-            this.disableAction(actionName);
-        }
+
+            this.ACTION.target.addEventListener("input-controller:action-activated", (action) => {
+                console.log("Button pressed: " + this.ACTION_PRESSED);
+                this.isKeyPressed(this.ACTION.keyCode);
+                console.log("Button activated: " + this.ACTION.keyCode);
+            });
+        } 
     }
 
     disableAction(actionName) {
@@ -36,28 +41,27 @@ static ACTION_DEACTIVATED = "input-controller:action-deactivated";
             const eventDeactivate = new Event(inputController.ACTION_DEACTIVATED);
             this.ACTION.target.dispatchEvent(eventDeactivate, {bubbles: true});
             this.detach();
+
+            this.ACTION.target.addEventListener("input-controller:action-deactivated", (action) => {
+                this.isKeyPressed(this.ACTION.keyCode);
+                console.log("Button pressed: " + this.ACTION_PRESSED);
+                console.log("Button diactivated: " + this.ACTION.keyCode);
+            });
         }     
     }
 
     attach(target) {
-        const buttonAdd = document.getElementById("btnAdd");
-        const input = document.getElementById("input1");
-        
-        target.target.addEventListener("input-controller:action-activated", (action) => {
-            this.isKeyPressed(target);
-            console.log("Button activated: " + this.ACTION.keyCode);
+        document.addEventListener("keydown", (action) => {
+            this.bindAction(action);
         });
-
+        document.addEventListener("keyup", (action) => {
+            this.bindAction(action)
+        });
     }
 
     detach() {
-        const target = this.ACTION;
-
-        target.target.addEventListener("input-controller:action-deactivated", (action) => {
-            this.isKeyPressed(target);
-            console.log("Button diactivated: " + this.ACTION.keyCode);
-        });
-
+        document.removeEventListener("keydown", (action) => {});
+        document.removeEventListener("keyup", (action) => {});
     }
 
     isActionActive(action) {
@@ -65,20 +69,7 @@ static ACTION_DEACTIVATED = "input-controller:action-deactivated";
     }
 
     isKeyPressed(keyCode) {
-        try {
-            if (keyCode.type === "keydown") {
-                this.ACTION_LISTENER = true;
-                console.log("Button pressed");
-                return true;
-            } 
-            if (keyCode.type === "keyup") {
-                this.ACTION_LISTENER = false;
-                console.log("Button unpressed");
-                return false;
-            }
-        } catch {
-        }
-        return false;
+        return this.ACTION_PRESSED = this.ACTION_PRESSED != true;
     }
 
 
